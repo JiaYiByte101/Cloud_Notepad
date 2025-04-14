7f5fa9bb97b2dc2faff628a7f6a2cd32fd4f61e4
zeng-yk <1721939591@qq.com>
2025-04-14 20:08:46 +0800

    增加验证码功能，修复一个bug

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,8 +7,9 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('login/', views.custom_login, name='login'),
     path('logout/', views.logout_confirm, name='logout'),
     path('profile/', views.profile, name='profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),  # 暂时注释掉
+    path('refresh-captcha/', views.refresh_captcha, name='refresh_captcha'),
 ]
